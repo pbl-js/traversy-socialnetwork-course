4950e2708aa8a45482d255b6de86095c59132206
Paweł Miłczak <jogobello4@gmail.com>
2019-09-23 14:57:01 +0200

    46 Starting on the dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -99,5 +100,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout / Clear Profile
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
